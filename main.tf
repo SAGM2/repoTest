77929731d2a16d81fbba878b280f10b82f557fcd
SAGM2 <68253615+SAGM2@users.noreply.github.com>
2020-09-03 12:00:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_rds_cluster" "default" {
   cluster_identifier      = "aurora-cluster-demo"
   engine                  = "aurora-mysql"
   engine_version          = "5.7.mysql_aurora.2.03.2"
-  availability_zones      = ["us-east-1"]
+  availability_zones      = "us-east-1"
   database_name           = "mydb"
   master_username         = "foo"
   master_password         = "bar"
