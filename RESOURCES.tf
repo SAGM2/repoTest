366107b5e1e4d59c8f8fe3d4fa35140c73297d2e
SAGM2 <68253615+SAGM2@users.noreply.github.com>
2020-09-03 15:19:42 -0500

    Create RESOURCES.tf

---
+++ RESOURCES.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_rds_cluster" "default" {
+  cluster_identifier      = "aurora-cluster-demo"
+  engine                  = "aurora-mysql"
+  engine_version          = "5.7.mysql_aurora.2.03.2"
+  database_name           = "mydb"
+  master_username         = "foo"
+  master_password         = "Mypassword123"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+}
+
+resource "aws_s3_bucket" "chp19" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
